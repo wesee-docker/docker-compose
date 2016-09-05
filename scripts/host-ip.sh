69c5e2cc351b6bbca595368fb4f7d71481faa33c
billqiang <whenjonny@gmail.com>
2016-09-05 13:05:59 +0800

    host

--- scripts/host-ip.sh
+++ scripts/host-ip.sh
@@ -2,10 +2,10 @@ ip=`ifconfig -a|grep inet|grep 10|grep -v 127.0.0.1|grep -v inet6|awk '{print $2
 
 echo "
 mysql -uroot -proot -h$ip < /data/compose/backup/create-db.sql;
-mysql -uroot -proot -h$ip -d webuy < /data/compose/backup/webuy-opt.sql;
-mysql -uroot -proot -h$ip -d wemake < /data/compose/backup/wemake-opt.sql;
-mysql -uroot -proot -h$ip -d user < /data/compose/backup/user-opt.sql;
-mysql -uroot -proot -h$ip -d order < /data/compose/backup/order-opt.sql;
-mysql -uroot -proot -h$ip -d upload < /data/compose/backup/upload-opt.sql;
-mysql -uroot -proot -h$ip -d moman < /data/compose/backup/moman-opt.sql;
+mysql -uroot -proot -h$ip -D webuy < /data/compose/backup/webuy-opt.sql;
+mysql -uroot -proot -h$ip -D wemake < /data/compose/backup/wemake-opt.sql;
+mysql -uroot -proot -h$ip -D user < /data/compose/backup/user-opt.sql;
+mysql -uroot -proot -h$ip -D order < /data/compose/backup/order-opt.sql;
+mysql -uroot -proot -h$ip -D upload < /data/compose/backup/upload-opt.sql;
+mysql -uroot -proot -h$ip -D moman < /data/compose/backup/moman-opt.sql;
 " > /data/compose/scripts/install-mariadb.sh
